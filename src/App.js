f25c0b2997b84035a73587bd790369d3935f0f9f
devansh <devansh.singh@mail.vinove.com>
2021-03-27 15:02:04 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ResturantData from "./restaurants.json";
 import cuisines from "./cuisines.json";
 import MainModule from "./MainModule";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import {DropdownButton,Dropdown} from 'react-bootstrap'
 function App() {
   const [SearchData, setSearchData] = useState("");
   const [Input, setInput] = useState([""]);
 
   const dispatch = useDispatch();
+  const Category = useSelector((state) => state.Category);
 
   useEffect(() => {
     dispatch({ type: "FETCH_REST", item: ResturantData[0].data });
@@ -20,7 +22,10 @@ function App() {
 
     setInput(temp.map((res) => res.value));
   };
-
+ 
+let data=Category.map((items)=>{
+  return items.name
+})
   return (
     <div className="container">
       <input
@@ -33,13 +38,20 @@ function App() {
       />
       <br />
       <select multiple={true} onChange={(e) => handleChange(e)}>
-        <option value="Chinese">Chinese</option>
+        {/* <option value="Chinese">Chinese</option>
         <option value="Indian">Indian</option>
         <option value="Thai">Thai</option>
         <option value="Greek">Greek</option>
         <option value="Vietnamese">Vietnamese</option>
-        <option value="French">French</option>
+        <option value="French">French</option> */}
+        {data.map(item=>(
+          console.log(item),
+           <option type='checkbox' value={item}>{item}</option>
+
+           ))}
+        
       </select>
+     
       <button onClick={() => dispatch({ type: "CLEAR" })}>Reset</button>
 
       <MainModule SearchData={SearchData} Input={Input} />
